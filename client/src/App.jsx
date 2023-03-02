b30d6dd6e606e64cc995e9555479a5986a65a6ee
Samatar5 <samatar5mohamed@gmail.com>
2023-03-02 13:38:11 +0100

    init

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import "./App.css";
 
 export default function App() {
-  return <div>App</div>;
+  const [channels, setChannels] = useState(null);
+  useEffect(() => {
+    async function getChannels() {
+      const response = await fetch("http://localhost:3000/channels");
+      const data = await response.json();
+      console.log(data);
+      setChannels(data);
+    }
+    getChannels();
+  });
+
+  return (
+    <div className="main-container">
+      <div className="sidebar">
+        {channels
+          ? channels.map((channel) => <div>{channel.name}</div>)
+          : "loading..."}
+      </div>
+      <div className="chat-section">chat section</div>
+    </div>
+  );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,35 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
+import cors from "cors";
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
   res.send("hej då");
 });
 
-app.listen(3000);
+const channelsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxlength: 30,
+    },
+  },
+  { timestamps: true }
+);
+
+const Channel = mongoose.model("channels", channelsSchema);
+
+app.get("/channels", async (req, res) => {
+  //   await Channel.insertMany([{ name: "batch 5" }, { name: "general" }]);
+  const channels = await Channel.find();
+  res.send(channels);
+});
+
+app.listen(3000, async () => {
+  await mongoose.connect(
+    "mongodb+srv://samatar:samatar@cluster0.um40t1s.mongodb.net/?retryWrites=true&w=majority"
+  );
+});
